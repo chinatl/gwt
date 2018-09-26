d46e885a4fb90551d8b867fe454b18b5f891df0b
zbj <you@example.com>
2018-09-26 17:31:33 +0800

    部门云盘

--- src/views/cloud-disk/company.vue
+++ src/views/cloud-disk/company.vue
@@ -338,29 +338,49 @@ export default {
       var data = {
         fileIds: this.fileIds.map(res => res + "").join(",")
       };
-      var object = Base64.encode(JSON.stringify(data));
-      var sign = md5(object + this.$store.getters.sign);
-      window.open(
-        `gwt/cloudisk/cloudiskAttachment/BatchDownload?${qs.stringify({
-          object,
-          sign,
-          token: this.$store.getters.token
-        })}`
-      );
+      var sigle_data = {
+        jh:parseInt(this.sigle_fileid)
+      }
+      if(this.fileIds.length === 1 && this.dirIds.length === 0){
+        // alert("dan")
+        //   console.log(parseInt(this.sigle_fileid))
+          var object = Base64.encode(JSON.stringify(sigle_data));
+          var sign = md5(object + this.$store.getters.sign);
+          window.open(
+            `/gwt/cloudisk/cloudiskAttachment/sigleFileDownload?${qs.stringify({
+              object,
+              sign,
+              token: this.$store.getters.token
+            })}`
+          );
+      }else{
+        // alert("duo")
+          var object = Base64.encode(JSON.stringify(data));
+          var sign = md5(object + this.$store.getters.sign);
+          window.open(
+            `/gwt/cloudisk/cloudiskAttachment/BatchDownload?${qs.stringify({
+              object,
+              sign,
+              token: this.$store.getters.token
+            })}`
+          );
+      }
     },
     //删除
     delete_btn() {
-      if(parseInt(this.current_user.id) !== this.createId && this.createId.includes(parseInt(this.current_user.id))){
-            this.$swal({
-              title: "提示信息！",
-              text: "您没有权限删除此文件",
-              type: "warning",
-              confirmButtonColor: "#DD6B55",
-              confirmButtonText: "确定",
-              showConfirmButton: true
-            });
-            return
-      }
+      // console.log(this.createId)
+      // return;
+      // if(parseInt(this.current_user.id) !== this.createId && this.createId.includes(parseInt(this.current_user.id))){
+      //       this.$swal({
+      //         title: "提示信息！",
+      //         text: "您没有权限删除此文件",
+      //         type: "warning",
+      //         confirmButtonColor: "#DD6B55",
+      //         confirmButtonText: "确定",
+      //         showConfirmButton: true
+      //       });
+      //       return
+      // }
       if (this.fileIds.length == 0 && this.dirIds.length == 0) {
         this.$swal({
           title: "提示信息！",
